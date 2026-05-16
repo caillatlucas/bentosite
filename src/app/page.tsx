c9f8115321f0c2637cf60f9e21c14f47ae1d449a
livrimfr <contact.livrim@gmail.com>
2026-05-16 21:39:19 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -48,6 +48,7 @@ export default function Home() {
     musicEnabled: false, 
     musicUrl: "", 
     musicCover: "",
+    primaryColor: "#ff3131",
     show3DBackground: false,
     musicRotationEnabled: true,
     sectionsConfig: [
