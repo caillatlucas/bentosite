258a5d1caa09ded63ea5ed46f5d0f48bf25fc255
caillatlucas <caillatlucas2304@gmail.com>
2026-05-31 13:19:51 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -7,5 +7,8 @@ const nextConfig = {
   basePath: '/bentosite',
   trailingSlash: true,
 };
+  basePath: '/bentosite',
+  output: 'export', // si vous utilisez l'export statique
+};
 
 export default nextConfig;
